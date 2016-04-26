b88b4a3a5a9fa081245bd67175593f6aa404085b
ChefCitron <florent.cabric@yahoo.fr>
2016-04-26 11:11:38 +0200

    "last pom"

--- TransformationTTS/src/main/java/Factory/EnableFactory.java
+++ TransformationTTS/src/main/java/Factory/EnableFactory.java
@@ -4,6 +4,7 @@ import org.yakindu.sct.model.sgraph.CompositeElement;
 import org.yakindu.sct.model.sgraph.Region;
 import org.yakindu.sct.model.sgraph.SGraphFactory;
 import org.yakindu.sct.model.sgraph.State;
+import org.yakindu.sct.model.sgraph.Transition;
 //import org.yakindu.sct.model.sgraph.*;
 import org.yakindu.sct.model.sgraph.impl.SGraphFactoryImpl;
 import org.eclipse.emf.common.util.EList;
@@ -13,18 +14,24 @@ import hamsters.HamstersOperator;
 
 public class EnableFactory {
 
-	private static SGraphFactory sgraph = new SGraphFactoryImpl();
+	private static SGraphFactory sgraph = SGraphFactory.eINSTANCE;
 	
 	public static State enabletoSc(HamstersOperator hOP) {
-		State s = sgraph.createState();
-		CompositeElement ce;
-		ce = sgraph.createState();
 		State e = sgraph.createState();
 		e.isComposite();
 		Region r = sgraph.createRegion();
-		ce.getRegions.add(r);
+		State temp = null;
+		Transition t = null;
 		for( int i = 0 ; i < hOP.getChildren().size() ; i++) {
-			
+			temp = sgraph.createState();
+			temp.setName(hOP.getChildren().get(i).toString());
+			r.getVertices().add(sgraph.createState());
+			if(i!=0){
+				t = sgraph.createTransition();
+				t.setSource(r.getVertices().get(i-1));
+				t.setTarget(r.getVertices().get(i));
+			}
 		}
+		
 	}
 }
