b2af7b1b5070655f0d454cda32e497b7d411a21a
ChefCitron <florent.cabric@yahoo.fr>
2016-04-28 16:03:40 +0200

    d

--- TransformationTTS/src/main/java/Factory/ChoiceFactory.java
+++ TransformationTTS/src/main/java/Factory/ChoiceFactory.java
@@ -13,17 +13,32 @@ public class ChoiceFactory extends FactoryTransformation {
 	private static SGraphFactory sgraph = SGraphFactory.eINSTANCE;
 
 	public static State choiceToSc(HamstersOperator hOP) {
+		// création de l'état composite initial
 		State e = sgraph.createState();
 		e.isComposite();
+		e.setName(hOP.getParent().getDescription());
+		
+		// création de la région de cet état composite
 		Region r = sgraph.createRegion();
-		Entry ent = sgraph.createEntry();
 		e.getRegions().add(r);
+		
+		// création du point d'entrée de la région link au premier état qui ammorce le choix
+		Entry ent = sgraph.createEntry();
+		r.getVertices().add(ent);
+		
+		// création de l'état ammorcant le choix
 		State initialChoiceState = (State) sgraph.createChoice();
 		r.getVertices().add(initialChoiceState);
+		
+		// création de la transition entre l'entry et l'état ammorcant le choix
+		Transition firstTransition = sgraph.createTransition();
+		firstTransition.setSource(ent);
+		firstTransition.setTarget(initialChoiceState);
+		// variable concernant les différents fils du choix 
 		State temp = null;
 		Transition t = null;
-		Transition firstTransition = sgraph.createTransition();
 		
+		// création des fils et de ses transitions avec le parent 
 		for (int i = 0; i < hOP.getChildren().size(); i++) {
 			temp = sgraph.createState();
 			temp.setName(hOP.getChildren().get(i).getDescription());
