fe2bea464f38a8a251790dec2cb56b330c8213a7
Irodium <jeff_perio@hotmail.fr>
2016-05-01 15:06:16 +0200

    Order Independant 3 sons

--- TransformationTTS/src/main/java/taskModelCreation/OrderIndependent.java
+++ TransformationTTS/src/main/java/taskModelCreation/OrderIndependent.java
@@ -25,16 +25,21 @@ public class OrderIndependent {
 			HamstersTask task2 = new HamstersTask();
 			task2.setType(TaskType.USER);
 			task2.setHelpText("Tache noeud numero 2");
+			HamstersTask task3 = new HamstersTask();
+			task3.setType(TaskType.USER);
+			task3.setHelpText("Tache noeud numero 3");
 			operator.addChild(task1);
 			operator.addChild(task2);
-			HamstersTask task3 = new HamstersTask();
-			task1.setType(TaskType.USER);
-			task1.setHelpText("Tache Feuille numero 1");
-			HamstersTask task4 = new HamstersTask();
-			task2.setType(TaskType.USER);
-			task2.setHelpText("Tache Feuille numero 2");
-			task1.addChild(task3);
-			task2.addChild(task4);
+			operator.addChild(task3);
+			
+//			HamstersTask task3 = new HamstersTask();
+//			task3.setType(TaskType.USER);
+//			task3.setHelpText("Tache Feuille numero 1");
+//			HamstersTask task4 = new HamstersTask();
+//			task4.setType(TaskType.USER);
+//			task4.setHelpText("Tache Feuille numero 2");
+//			task1.addChild(task3);
+//			task2.addChild(task4);
 		}
 		public HamstersAPI getAPI() {
 			return hamAPI;
